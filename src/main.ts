4a8ec7f7b9526278b9639ab5101663404d0a9693
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2024-12-16 19:04:13 +0530

    added SSO

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { AccountManager } from './sso/authConfig';
+import { makeGraphRequest } from './sso/msgraph-helper';
 
-Office.initialize = () => {
-  bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+const accountManager = new AccountManager();
+
+Office.onReady(async (info) => {
+  console.log(`Add-in is ready for ${info.host} on ${info.platform}`);
+
+  // Initialize when Office is ready.
+  if (info.host === Office.HostType.Outlook) {
+    try {
+      // Initialize MSAL and ensure it's ready before proceeding.
+      await accountManager.initialize();
+      console.log("AccountManager initialized successfully.");
+      
+      // Proceed with SSO only after successful initialization.
+      await SSO();
+    } catch (error) {
+      console.error("Failed to initialize AccountManager:", error);
+    }
+  }
+
+});
+
+// SSO
+async function SSO() {
+  const accessToken = await accountManager.ssoGetAccessToken(["user.read"]);
+  
+  const response: { displayName: string; mail: string } = await makeGraphRequest(accessToken, "/me", "");
+  console.log(`Hello, ${response.displayName}!`);
+
+  if(accessToken) {
+    bootstrapApplication(AppComponent, appConfig)
+    .catch((err) => console.error(err));
+  }
 }
